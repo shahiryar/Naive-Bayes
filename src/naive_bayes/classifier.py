50569c27f7316841d1cd4b55f94ff4817b8088ff
Shahiryar Saleem <shahiryarsaleem@gmail.com>
2023-04-30 00:52:54 +0500

    added docstring to the GaussianNB class

--- src/naive_bayes/classifier.py
+++ src/naive_bayes/classifier.py
@@ -1,8 +1,33 @@
-
 import numpy as np
 from scipy.stats import norm
 
 class GaussianNB:
+    """
+    A Gaussian Naive Bayes classifier.
+
+    Parameters
+    ----------
+    None
+
+    Attributes
+    ----------
+    classes : ndarray of shape (n_classes,)
+        The unique class labels observed in the training data.
+    class_priors : ndarray of shape (n_classes,)
+        The prior probabilities of each class.
+    mean : ndarray of shape (n_classes, n_features)
+        The mean of the feature values for each class.
+    variance : ndarray of shape (n_classes, n_features)
+        The variance of the feature values for each class.
+
+    Methods
+    -------
+    fit(X, y)
+        Fit the Gaussian Naive Bayes model to the training data.
+    predict(X)
+        Predict the class labels of the input data using the fitted model.
+    """
+
     def __init__(self):
         self.classes = None
         self.class_priors = None
@@ -10,6 +35,21 @@ class GaussianNB:
         self.variance = None
         
     def fit(self, X, y):
+        """
+        Fit the Gaussian Naive Bayes model to the training data.
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            The training data.
+        y : ndarray of shape (n_samples,)
+            The target values.
+
+        Returns
+        -------
+        self : GaussianNB
+            The fitted model.
+        """
         self.classes = np.unique(y)
         n_classes = len(self.classes)
         n_features = X.shape[1]
@@ -24,6 +64,19 @@ class GaussianNB:
             self.variance[i, :] = X_c.var(axis=0)
         
     def predict(self, X):
+        """
+        Predict the class labels of the input data using the fitted model.
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            The input data.
+
+        Returns
+        -------
+        y_pred : ndarray of shape (n_samples,)
+            The predicted class labels.
+        """
         y_pred = np.zeros(X.shape[0])
         
         for i, x in enumerate(X):
